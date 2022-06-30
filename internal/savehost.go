152c069fa4cb307ecf4033554da73b5b9818e07e
Onumah Kalu Samuel <onumahkalusamuel@gmail.com>
2022-06-30 06:39:32 +0100

    update

--- internal/savehost.go
+++ internal/savehost.go
@@ -9,6 +9,23 @@ import (
 
 func SaveHost(host string) {
 
+	host = strings.Split(host, ":")[0]
+	host = strings.Trim(host, "www.")
+
+	// if host lent is more than three pick, the last three
+	hostparts := strings.Split(host, ".")
+	length := len(hostparts)
+	if length > 3 {
+		host = hostparts[length-1] + hostparts[length-2] + hostparts[length-3]
+	}
+
+	// its more than 4 just skip
+	if length > 4 {
+		return
+	}
+
+	// if len(hostparts)
+
 	var exists bool
 	var filecontent []byte
 	var err error
